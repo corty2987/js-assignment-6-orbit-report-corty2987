f98b6b21fd478a6cc420e09b54968cf71419d2a5
corty2987 <cortyblackwell@gmail.com>
2023-03-11 15:18:55 -0600

    First commit with part 1 done

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,13 +7,20 @@ import { Satellite } from './satellite';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'orbit-report';
+	// @Input() satellite = "";
+	title = 'orbit-report';
 
   sourceList: Satellite[];
   displayList: Satellite[];
 
 	constructor() {
-		this.sourceList = [];
+		this.sourceList = [
+			new Satellite("SiriusXM", "Communication", "2009-03-21", "LOW", true),
+			new Satellite("Cat Scanner", "Imaging", "2012-01-05", "LOW", true),
+			new Satellite("Weber Grill", "Space Debris", "1996-03-25", "LOW", false),
+			new Satellite("GPS 938", "Positioning", "2001-11-01", "HIGH", true),
+			new Satellite("ISS", "Space Station", "1998-11-20", "LOW", true),
+		];
 		this.displayList = [];
 		let satellitesUrl = 'https://handlers.education.launchcode.org/static/satellites.json';
 
